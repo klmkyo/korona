5ae320b3a396ed7e61b6966cc788ee19489d290e
klmkyo <mklimek03@gmail.com>
2020-08-09 18:37:41 +0200

    ay i think its working

--- script.py
+++ script.py
@@ -16,32 +16,40 @@ import csv
 import json
 import os
 
-# Curve base:
-points = [[0, 0], [0, 2], [2, 3], [4, 0], [6, 3], [8, 2], [8, 0]]
-points = np.array(points)
+def downloadFile(docid, cookies, location):
+    """
+    cookies = {
+    '.AspNetCore.Antiforgery.ylPDrIszQPI': str(a['value']),
+    'ARRAffinity': str(d['value']),
+    'visid_incap_2280950': str(e['value']),
+    'incap_ses_408_2280950': str(f['value']),
+    '.AspNetCore.Cookies': str(c['value']),
+    '.AspNetCore.Session': str(b['value']),
+    }"""
 
-x = points[:,0]
-y = points[:,1]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Upgrade-Insecure-Requests': '1',
+        'Connection': 'keep-alive',
+    }
 
+    params = (
+        ('documentid', str(docid)),
+    )
 
-t = range(len(points))
-ipl_t = np.linspace(0.0, len(points) - 1, 10)
-
-x_tup = si.splrep(t, x, k=3)
-y_tup = si.splrep(t, y, k=3)
-
-x_list = list(x_tup)
-xl = x.tolist()
-x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
-
-y_list = list(y_tup)
-yl = y.tolist()
-y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
-
-x_i = si.splev(ipl_t, x_list) # x interpolate values
-y_i = si.splev(ipl_t, y_list) # y interpolate values
-
-##actual script
+    rawresponse = requests.get('https://wyniki.diag.pl/Document/GetPdf', headers=headers, params=params, cookies=cookies)
+    
+    data = rawresponse.text
+    with open(location, 'w') as f:
+        f.write(data)
+        if os.stat(location).st_size > 1024 * 10:
+            success = 1
+        else:
+            print(str(line) + ': ' + '[Error] Próbowano pobrać, jednak pobrany plik jest podejrzanie małego rozmiaru {} ({}, {})'.format(name, pesel, barcode))
+            success = 0
+    return(success)
 
 browser = webdriver.Firefox()
 browser.install_addon("/home/klmkyo/korona/buster_captcha_solver_for_humans-1.0.1-an+fx.xpi")
@@ -84,15 +92,6 @@ for line, x in enumerate(dane):
 
         elem = browser.find_element(By.ID, 'Barcode')
         elem.send_keys(barcode + Keys.RETURN)
-
-        #ruszanie myszką
-        if False:
-            action = ActionChains(browser)
-            action.move_to_element(elem)
-            action.perform()
-            for mouse_x, mouse_y in zip(x_i, y_i):
-                action.move_by_offset(mouse_x,mouse_y)
-            action.perform()
         
         elem = browser.find_element(By.ID, 'BirthdaySingleOrderLoginInput')
         elem.send_keys(bdate + Keys.RETURN)
@@ -113,6 +112,13 @@ for line, x in enumerate(dane):
         d = browser.get_cookie('ARRAffinity')
         e = browser.get_cookie('visid_incap_2280950')
         f = browser.get_cookie('incap_ses_408_2280950')
+        
+        cookies = browser.get_cookies()
+
+        c = {}
+        for cookie in cookies:
+            c[cookie['name']] = cookie['value']
+            
         
         name = browser.find_element(By.ID, 'PatientNameAndSurname').text
         imie, nazwisko = name.split(' ')
@@ -142,37 +148,11 @@ for line, x in enumerate(dane):
                 browser.find_element(By.XPATH, '/html/body/main/div[3]/section[2]/div[3]/div/div[2]/div/div[3]/button')
                 docid = browser.find_element(By.XPATH, '/html/body/main/div[3]/section[2]/div[3]/div/div[2]/div/div[3]/button').get_attribute('data-docid')
                 
-                cookies = {
-                '.AspNetCore.Antiforgery.ylPDrIszQPI': str(a['value']),
-                'ARRAffinity': str(d['value']),
-                'visid_incap_2280950': str(e['value']),
-                'incap_ses_408_2280950': str(f['value']),
-                '.AspNetCore.Cookies': str(c['value']),
-                '.AspNetCore.Session': str(b['value']),
-                }
-
-                headers = {
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Connection': 'keep-alive',
-                }
-
-                params = (
-                    ('documentid', str(docid)),
-                )
-
-                rawresponse = requests.get('https://wyniki.diag.pl/Document/GetPdf', headers=headers, params=params, cookies=cookies)
-                
-                data = rawresponse.text
-                people.append(person)
-                with open('wyniki/' + wynik + '/' + slugify(name)+'.pdf', 'w') as f:
-                    f.write(data)
-                    if os.stat('wyniki/' + wynik + '/' + slugify(name)+'.pdf').st_size > 1024 * 10:
+                for i in range(0,3):
+                    if downloadFile(docid, c, 'wyniki/' + wynik + '/' + slugify(name)+'.pdf'):
+                        people.append(person)
                         processed.append(x[0] + ';' + x[1])
-                    else:
-                        print(str(line) + ': ' + '[Error] Próbowano pobrać, jednak pobrany plik jest podejrzanie małego rozmiaru {} ({}, {})'.format(name, pesel, barcode))
+                        break
                 
             except:
                 print(str(line) + ': ' + '[Error] Pobieranie wyników dla {} ({}, {}) nie powiodło się!'.format(name, pesel, barcode))
@@ -196,17 +176,21 @@ with open('export' + str(time.time()) + '.json', 'w') as j:
     j.write(json.dumps(people, ensure_ascii=False))
 
 rows = []
-with open('output.csv', 'r') as read: 
-    reader = csv.reader(read)
-    for row in reader:
-        rows.append(row)
+try:
+    with open('output.csv', 'r') as read: 
+        reader = csv.reader(read)
+        for row in reader:
+            rows.append(row)
+except:
+    pass
     
 with open('output.csv', 'a+') as output:
     writer = csv.writer(output)
     for person in people:
         isAlready = False
-        for x in rows: 
-            if person['barcode'] in x: isAlready = True
+        if rows:
+            for x in rows: 
+                if person['barcode'] in x: isAlready = True
         if not isAlready:
             v = []
             for key, value in person.items():
